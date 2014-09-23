c5f1548f4281b844649ff39e285ce1ffe3aa25bc
Mohsen Shafiei <mohsen.sh12@hotmail.com>
2014-09-23 09:57:52 +0330

    Add migrations to CheeTheme

---
+++ src/migrations/2014_09_23_062421_create_active_themes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActiveThemesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('active_themes', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 150);
+			$table->integer('theme_order')->default(0);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('active_themes');
+	}
+
+}
